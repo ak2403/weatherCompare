d01181a17e01545b823f1cf5897713f83a88167a
akspikey <arunpasupathi24@gmail.com>
2018-04-04 16:33:12 +0530

    Changes for placeholder and graph

--- main/src/actions/index.js
+++ main/src/actions/index.js
@@ -8,8 +8,8 @@ export const fetchCity = (details) => {
     // }
     return function (dispatch) {
         axios.all([
-            axios.get('http://api.openweathermap.org/data/2.5/weather?q=chennai&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
-            axios.get('http://api.openweathermap.org/data/2.5/forecast?q=chennai&APPID=a44e37bfe521db4bf31b0b57918362bf')
+            axios.get('http://api.openweathermap.org/data/2.5/weather?q='+details.city+'&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
+            axios.get('http://api.openweathermap.org/data/2.5/forecast?q='+details.city+'&APPID=a44e37bfe521db4bf31b0b57918362bf')
         ])
             .then(axios.spread(function (selectedCity, weatherList) {
                 dispatch({ type: FETCH_CITY, selectedCity: selectedCity.data, weatherList: weatherList.data });
