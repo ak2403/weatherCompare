882b44269c0eefd5455897b31fb765171c7fdd40
ak2403 <arunpasupathi24@gmail.com>
2018-04-13 19:03:54 +0530

    Handled Secure Http requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,8 @@ import { FETCH_CITY, FETCH_SECONDARY_CITY } from './types';
 export const fetchCity = (details) => {
     return function (dispatch) {
         axios.all([
-            axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + details.city + '&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
-            axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + details.city + '&APPID=a44e37bfe521db4bf31b0b57918362bf')
+            axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + details.city + '&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
+            axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + details.city + '&APPID=a44e37bfe521db4bf31b0b57918362bf')
         ])
             .then(axios.spread(function (selectedCity, weatherList) {
                 dispatch({ type: FETCH_CITY, selectedCity: selectedCity.data, weatherList: weatherList.data });
@@ -16,8 +16,8 @@ export const fetchCity = (details) => {
 export const fetchSecondaryCity = (details) => {
     return function (dispatch) {
         axios.all([
-            axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + details.city + '&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
-            axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + details.city + '&APPID=a44e37bfe521db4bf31b0b57918362bf')
+            axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + details.city + '&units=metric&APPID=a44e37bfe521db4bf31b0b57918362bf'),
+            axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + details.city + '&APPID=a44e37bfe521db4bf31b0b57918362bf')
         ])
             .then(axios.spread(function (selectedCity, weatherList) {
                 dispatch({ type: FETCH_SECONDARY_CITY, selectedCity: selectedCity.data, weatherList: weatherList.data });
